42c89048e7828d52b17af30f012622439195a313
Geneclecio <geneclecio.santos.22@gmail.com>
2023-10-10 22:03:44 -0300

    1010.5

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,16 +1,30 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button meuBotao;
+    EditText meuCampoDeTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        meuBotao = findViewById(R.id.containedButton);
+        meuCampoDeTexto = findViewById(R.id.textValor);
+
+        meuBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textoDigitado = meuCampoDeTexto.getText().toString();
+                // Aqui você pode armazenar 'textoDigitado' ou fazer o que desejar com ele.
+            }
+        });
+    }
 }
